b13496a9ddd564a36c2bd3de06ad84046afac219
BT_1N3_34 <ewu1985@bths.edu>
2019-02-11 10:59:27 -0500

    Actually merge the array

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -8,10 +8,27 @@ import java.util.stream.IntStream;
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+    public static int[] simpleMergeOld(int[] arr1, int[] arr2) {
         return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2))
                 .sorted()
                 .toArray();
     }
 
+    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int[] sortedArr = new int[arr1.length + arr2.length];
+
+        IntStream.range(0, arr1.length).forEach(i -> sortedArr[i] = arr1[i]);
+        IntStream.range(0, arr2.length).forEach(i -> sortedArr[arr1.length + i] = arr2[i]);
+
+        for (int i = 0; i < sortedArr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sortedArr[i] < sortedArr[j]) {
+                    int tmp = sortedArr[j];
+                    sortedArr[j] = sortedArr[i];
+                    sortedArr[i] = tmp;
+                }
+            }
+        }
+        return sortedArr;
+    }
 }
